0dafe7b2af79d807e8bd353f4d4df01e6871b5dc
AnaEstacheski <flaviaestacheski@gmail.com>
2022-12-13 11:45:53 -0300

    feat: CRUD de Jogos: Read

--- src/controllers/gamesController.js
+++ src/controllers/gamesController.js
@@ -16,12 +16,25 @@ export async function insertGame(req, res) {
 
 export async function getGames(req, res) {
     const { name } = req.query
+    if (name) {
+        try {
+            const gamesQuery = await connectionDB.query(
+                `SELECT games.*, categories.name AS "categoryName" 
+                FROM games JOIN categories ON games."categoryId" = categories.id 
+                WHERE LOWER(games.name) LIKE LOWER($1)`,
+                [`%${name}%`]
+            );
+            return res.send(gamesQuery.rows);
+        } catch (err) {
+            return res.sendStatus(400).send(err.message);
+        }
+    }
     try {
-       const games = await connectionDB.query(
-            "SELECT * FROM games;"
+        const games = await connectionDB.query(
+            `SELECT games.*, categories.name AS "categoryName" FROM games JOIN categories ON games."categoryId" = categories.id;`
         );
-        res.status(200).send(games);
-    } catch (err) {
-        res.sendStatus(500).send(err.message);
+        return res.send(games.rows);
+    } catch (error) {
+        return res.sendStatus(500).send(err.message);
     }
 }
